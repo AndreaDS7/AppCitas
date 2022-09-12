1cd88bda0a786e168caf0cc0b7f9043d5655496b
Marco <marcogaytan348@gmail.com>
2022-09-11 22:13:41 -0500

    Creating an Account Controller with a register endpoint

--- AppCitas.Service/Entities/AppUser.cs
+++ AppCitas.Service/Entities/AppUser.cs
@@ -1,9 +1,9 @@
-﻿namespace AppCitas.Service.Entities
-{
-    public class AppUser
-    {
-        public int Id { get; set; }
+﻿namespace AppCitas.Service.Entities;
 
-        public string UserName { get; set; }
-    }
+public class AppUser
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
