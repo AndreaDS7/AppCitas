bdfaf052e63505f5a955d5d5d647680b6c58b0bb
Marco <marcogaytan348@gmail.com>
2022-10-07 21:10:31 -0500

    Adding a token service

--- AppCitas.Service/Startup.cs
+++ AppCitas.Service/Startup.cs
@@ -1,19 +1,26 @@
 using AppCitas.Service.Data;
+using AppCitas.Service.Interfaces;
+using AppCitas.Service.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 namespace AppCitas;
+
 public class Startup
 {
     private readonly IConfiguration _config;
+
     public Startup(IConfiguration config)
     {
         _config = config;
     }
+
     public IConfiguration Configuration { get; }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddScoped<ITokenService, TokenService>();
         services.AddDbContext<DataContext>(options =>
         {
             options.UseSqlite(
@@ -28,6 +35,7 @@ public class Startup
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
         });
     }
+
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
@@ -37,6 +45,7 @@ public class Startup
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
         }
+
         app.UseHttpsRedirection();
 
         app.UseRouting();
